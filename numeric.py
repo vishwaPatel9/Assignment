1407135b377ceeb97bb90a03ecb42d40dee284f7
vishwaPatel9 <vishwaPatel9@gmail.com>
2025-12-27 13:04:46 +0530

    Remove extra Python files, keep only required ones

--- numeric.py
+++
@@ -1,7 +0,0 @@
-#print(max(1,2,3,4,5))
-
-#print(min(10,19,8,7,65))
-
-#print(abs(-2020))
-
-print(pow(2,3))
--- simple_interest.py
+++
@@ -1,5 +0,0 @@
-principal=float(input("Enter principal amount:"))
-rate=float(input("Enter interest rate:"))
-time=float(input("Enter time:"))
-si=(principal*rate*time)/100
-print("Simple interest is:",si)
